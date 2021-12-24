559bee0af3d37bc9f764b22db189da43d27dede6
Comp Master <kalam.balshyryn@gmail.com>
2021-12-24 19:46:46 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,62 @@
+import os
+import sys
+
+import pygame
+
+
+def load_image(name, colorkey=None):
+    fullname = os.path.join('data', name)
+    try:
+        image = pygame.image.load(fullname).convert()
+    except pygame.error as message:
+        print('Cannot load image:', name)
+        raise SystemExit(message)
+
+    # если файл не существует, то выходим
+    if not os.path.isfile(fullname):
+        print(f"Файл с изображением '{fullname}' не найден")
+        sys.exit()
+    image = pygame.image.load(fullname)
+    return image
+
+
+FPS = 50
+
+def terminate():
+    pygame.quit()
+    sys.exit()
+
+def start_screen():
+
+    intro_text = ["Главная страница",
+                  "Настройки",
+                  "Продолжить"]
+    size=WIDTH, HEIGHT= 700, 400
+    screen=pygame.display.set_mode(size)
+    clock=pygame.time.Clock()
+    fon = pygame.transform.scale(load_image('download.png'), (WIDTH, HEIGHT))
+    screen.blit(fon, (0, 0))
+    font = pygame.font.Font(None, 30)
+    text_coord = 100
+    for line in intro_text:
+        string_rendered = font.render(line, 1, pygame.Color('white'))
+        intro_rect = string_rendered.get_rect()
+        text_coord += 10
+        intro_rect.top = text_coord
+        intro_rect.x = 10
+        text_coord += intro_rect.height
+        screen.blit(string_rendered, intro_rect)
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                terminate()
+            elif event.type == pygame.KEYDOWN or \
+                    event.type == pygame.MOUSEBUTTONDOWN:
+                return  # начинаем игру
+        pygame.display.flip()
+        clock.tick(FPS)
+
+
+pygame.init()
+start_screen()
